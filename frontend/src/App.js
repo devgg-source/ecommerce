20a754fa6651e163de65f2eb64f1ebde79a907ed
devgg-source <ggkarthikmessi007@gmail.com>
2021-09-17 07:56:57 +0530

    Header and Footer component added..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Welcome </h1>
+      <Footer />
     </>
   );
 }
