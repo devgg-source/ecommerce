2ff4fd5b4167773d4ba81d1f9804b41e8c71694c
devgg-source <ggkarthikmessi007@gmail.com>
2021-10-06 12:21:20 +0530

    console log removed..

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,6 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-  console.log(user);
 
   if (user && (await user.matchPassword(password))) {
     res.json({
