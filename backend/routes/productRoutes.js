4ca362013c52134264652964443930ac1d16283f
devgg-source <ggkarthikmessi007@gmail.com>
2021-10-06 12:20:51 +0530

    create and update routes are added..

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,14 +4,17 @@ import {
   getProducts,
   getProductById,
   deleteProductById,
+  updateProduct,
+  createProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
 router
   .route("/:id")
   .get(getProductById)
-  .delete(protect, admin, deleteProductById);
+  .delete(protect, admin, deleteProductById)
+  .put(protect, admin, updateProduct);
 
 export default router;
