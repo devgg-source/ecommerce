b1c48a0bb55169ad970a7e1ae1c73ea75f826fda
devgg-source <ggkarthikmessi007@gmail.com>
2021-10-06 07:42:13 +0530

    delete product action added..

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,6 +6,8 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
@@ -35,6 +37,35 @@ export const listProductDetails = (id) => async (dispatch) => {
     dispatch({
       type: PRODUCT_DETAILS_SUCCESS,
       payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload: error.response?.data.message
+        ? error.response.data.message
+        : error.message,
+    });
+  }
+};
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_DELETE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    await axios.delete(`/api/products/${id}`, config);
+    dispatch({
+      type: PRODUCT_DELETE_SUCCESS,
     });
   } catch (error) {
     dispatch({
