dbd4990e949f7baaaf9b867c75b162bade484452
devgg-source <ggkarthikmessi007@gmail.com>
2021-10-07 18:15:04 +0530

    user added in reviews..

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -13,6 +13,11 @@ const reviewScheme = mongoose.Schema(
       type: String,
       required: true,
     },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
   {
     timestamps: true,
