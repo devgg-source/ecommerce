c0db2b246fa19394a6f507a335c0b3f0d6d45c8f
devgg-source <ggkarthikmessi007@gmail.com>
2021-10-09 13:15:11 +0530

    Row,Col added and search words removed after serach..

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 const SearchBox = ({ history }) => {
   const [keyword, setKeyword] = useState("");
@@ -8,23 +8,30 @@ const SearchBox = ({ history }) => {
     e.preventDefault();
     if (keyword.trim()) {
       history.push(`/search/${keyword}`);
+      setKeyword("");
     } else {
       history.push("/");
     }
   };
 
   return (
-    <Form onSubmit={submitHandler} inline>
-      <Form.Control
-        type="text"
-        name="q"
-        placeholder="Search Products.."
-        value={keyword}
-        onChange={(e) => setKeyword(e.target.value)}
-      ></Form.Control>
-      <Button type="submit" variant="outline-success" className="p-2">
-        Search
-      </Button>
+    <Form onSubmit={submitHandler} inline className="flex">
+      <Row>
+        <Col md={8}>
+          <Form.Control
+            type="text"
+            name="q"
+            placeholder="Search Products.."
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+          ></Form.Control>
+        </Col>
+        <Col sm={2}>
+          <Button type="submit" variant="outline-success" className="p-2">
+            Search
+          </Button>
+        </Col>
+      </Row>
     </Form>
   );
 };
